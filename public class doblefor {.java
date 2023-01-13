607a61656144c5145d26db3c3ffa96e52469df67
miguelrubio96 <miguelrubioru@gmail.com>
2023-01-13 09:53:06 +0100

    he cambiado un 2 por un 3

---
+++ public class doblefor {.java
@@ -0,0 +1,27 @@
+public class doblefor {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+		int columnas = 10;
+		int filas = 4;
+		int c=0;
+		for (int z = 0; z < 3; z++) {
+			while (z == 0 || z == 1) {
+				for (int k = 0; k < 3; k++) {
+					for (int i = 0; i < filas; i++) {
+						for (int j = 0; j < columnas; j++) {
+							if (j % 2 == 0 && j % 4 == 0)
+								
+								System.out.print(j);
+							else
+								System.out.print("m");
+						}
+						System.out.println();
+					}
+					System.out.println(c++);
+				}
+			}
+		}
+	}
+
+}
